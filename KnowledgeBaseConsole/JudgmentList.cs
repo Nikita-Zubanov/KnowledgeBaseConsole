a3b15cd6eec2e642a5763ac4faa39886020ce38a
Никита Зубанов <just-a-fan@mail.ru>
2020-03-18 22:25:43 +0400

    Для более понятной структуры были воспроизведены следующие косметические действия: 1) класс Antecedent переименован в JudgmentList. JudgmentList не хранит свойства суждения, а является лишь хранилищем для массива суждений. А также переопределяет Equals (это была единственная причина оставить этот класс, а не использовать обычный List) 2) класс Consequent удален 3) абстрактный класс Judgment изменен на обычный

---
+++ KnowledgeBaseConsole/JudgmentList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KnowledgeBaseConsole
+{
+    class JudgmentList
+    {
+        private IList<Judgment> judgments;
+
+        public IList<Judgment> Judgments { get { return this.judgments; } }
+
+        public JudgmentList(Judgment judgment)
+        {
+            this.judgments = new List<Judgment> { judgment };
+        }
+
+        public JudgmentList(IList<Judgment> judgments) 
+        {
+            this.judgments = judgments;
+        }
+
+        public /*new*/ bool Equals(object obj)
+        {
+            if (obj is JudgmentList)
+            {
+                JudgmentList antecedent = obj as JudgmentList;
+
+                foreach (Judgment currentJudgment in this.judgments)
+                {
+                    Boolean hasJudgment = false;
+
+                    foreach (Judgment judgment in judgments)
+                        if (currentJudgment.Equals(judgment))
+                        {
+                            hasJudgment = true;
+                            break;
+                        }
+
+                    if (!hasJudgment)
+                        return false;
+                }
+
+                return true;
+            }
+            else
+                throw new ArgumentException();
+        }
+    }
+}
